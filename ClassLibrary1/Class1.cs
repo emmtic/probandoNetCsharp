53fddde0a39a54b2f58d79d5c7642aad20ed4249
emmtic <gitemmtic@gmail.com>
2019-11-02 15:24:28 -0300

    cuarto commit

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace ClassLibrary1
 {
     public class Class1
     {
         private string nombre;
+        private string apellido;
     }
 }
